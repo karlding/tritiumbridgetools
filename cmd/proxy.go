4ac00f38b365c5806e288cb31cf7e5bf319fa0b8
Karl <karlding@users.noreply.github.com>
2019-02-04 16:24:46 -0500

    Refactor goroutines for handling Tritium messages

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -34,11 +34,7 @@ type Config struct {
 // with the Tritium CAN-Ethernet bridge
 var Transport string
 
-// InterfaceName is the interface to listen on
-var InterfaceName string
-
-var bridgeAddress string
-var bridge map[string]string
+var tomlFile string
 
 func init() {
 	log.SetOutput(os.Stdout)
@@ -48,14 +44,8 @@ func init() {
 	proxyCommand.Flags().StringVarP(&Transport, "transport", "t", "", "Transport source [tcp,udp]")
 	proxyCommand.MarkFlagRequired("transport")
 
-	proxyCommand.Flags().StringVarP(&InterfaceName, "interface", "i", "", "Network interface to listen on")
-	proxyCommand.MarkFlagRequired("interface")
-
-	proxyCommand.Flags().StringToStringVar(&bridge, "bridge", nil, "Bridge mapping")
-	proxyCommand.MarkFlagRequired("bridge")
-
-	proxyCommand.Flags().StringVarP(&bridgeAddress, "bridgeaddress", "p", "", "Bridge IP address")
-	proxyCommand.MarkFlagRequired("bridgeaddress")
+	proxyCommand.Flags().StringVarP(&tomlFile, "config", "f", "", "TOML file")
+	proxyCommand.MarkFlagRequired("config")
 }
 
 var proxyCommand = &cobra.Command{
@@ -68,20 +58,49 @@ var proxyCommand = &cobra.Command{
 	},
 }
 
-func doStuffOverUDP(fd int, networkInterface *net.Interface) {
-	addrs, _ := networkInterface.Addrs()
-	for _, addr := range addrs {
-		log.Printf("UDP IP: %s\n", addr.String())
+func handleUDPPackets(packetConn *ipv4.PacketConn, socketMap map[uint8]int) {
+	// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
+	b := make([]byte, 30)
+
+	for {
+		numBytes, _, _, err := packetConn.ReadFrom(b)
+		if err != nil {
+			// error handling
+			continue
+		}
+
+		if numBytes != 30 {
+			log.Println(b)
+			panic("Failed")
+		}
+
+		tritiumPacket := new(tritium.Packet)
+		tritium.ByteArrayToTritiumMessage(b, tritiumPacket)
+
+		// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
+		if !tritiumPacket.FlagHeartbeat {
+			sendFrame := make([]byte, 16)
+			tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
+
+			// Find the socket by bus number
+			if fd, ok := socketMap[tritiumPacket.BusNumber]; ok {
+				unix.Write(fd, sendFrame)
+			}
+		}
 	}
+}
+
+func doStuffOverUDP(conf Config) {
 	// The Group Address is 239.255.60.60
 	group := net.IPv4(239, 255, 60, 60)
 
+	// socketMap[Bus Number] = SocketCAN file descriptor
+	socketMap := make(map[uint8]int)
+
 	// Start a UDP connection
 	// The Tritium CAN-Ethernet bridge always broadcasts on port 4876
-	// TODO: Find the IP address on the provided interface
-	// MulticastAddrs
+	// TODO: Should we only bind on a single interface?
 	c, err := net.ListenPacket("udp4", "0.0.0.0:4876")
-	// Otherwise we can use TCP
 	if err != nil {
 		// error handling
 		return
@@ -92,46 +111,41 @@ func doStuffOverUDP(fd int, networkInterface *net.Interface) {
 	// This can be verified by checking the groups you belong to:
 	// 	netstat -gn | grep '239.255.60.60'
 	p := ipv4.NewPacketConn(c)
-	if err := p.JoinGroup(networkInterface, &net.UDPAddr{IP: group}); err != nil {
-		// error handling
-		return
+
+	for _, bridge := range conf.Bridge {
+		vcan, err := net.InterfaceByName(bridge.SocketCANInterface)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		fd, err := unix.Socket(unix.AF_CAN, unix.SOCK_RAW, unix.CAN_RAW)
+		if err != nil {
+			return
+		}
+
+		addr := &unix.SockaddrCAN{Ifindex: vcan.Index}
+		socketMap[bridge.ID] = fd
+		unix.Bind(fd, addr)
+
+		networkInterface, err := net.InterfaceByName(bridge.NetworkInterface)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		if err := p.JoinGroup(networkInterface, &net.UDPAddr{IP: group}); err != nil {
+			// error handling
+			return
+		}
 	}
 
-	go func() {
-		b := make([]byte, 30)
-		for {
-			// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
-			numBytes, _, addr, err := p.ReadFrom(b)
-			log.Printf("Received %d bytes\n", numBytes)
-			log.Printf("Address: %s\n", addr.String())
-			if err != nil {
-				// error handling
-				continue
-			}
-
-			if numBytes != 30 {
-				log.Println(b)
-				panic("Failed")
-			}
-
-			tritiumPacket := new(tritium.Packet)
-			tritium.ByteArrayToTritiumMessage(b, tritiumPacket)
-
-			// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
-			if !tritiumPacket.FlagHeartbeat {
-				sendFrame := make([]byte, 16)
-				tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
-
-				// Find the socket by bus number
-				unix.Write(fd, sendFrame)
-			}
-		}
-	}()
+	go handleUDPPackets(p, socketMap)
 
 	select {}
 }
 
-func doStuffOverTCP(fd int, networkInterface *net.Interface) {
+func doStuffOverTCP(conf Config) {
 	// In TCP mode, we need to send:
 	//
 	// +-----------------------------+
@@ -147,130 +161,6 @@ func doStuffOverTCP(fd int, networkInterface *net.Interface) {
 	// +-----------------------------+
 	// | Client Identifier (56 bits) |
 	// +-----------------------------+
-	setupBuffer := make([]byte, (4 + 4 + 1 + 7 + 1 + 7))
-
-	// The bridge will forward any packet matching:
-	//
-	// 		fwdIdentifier <= CAN arbitration id < (fwdIdentifier + fwdRange)
-	//
-	// So we set the bridge to forward all valid CAN IDs (including extended),
-	// which is the range [0, 2^29 - 1]
-	fwdIdentifier := uint32(0)
-	fwdRange := uint32(536870911)
-	// The Bus Number must match the Bus Number specified in the Tritium
-	// CAN-Ethernet Bridge Configuration tool, otherwise the TCP connection is
-	// terminated.
-	// TODO: Take these as configuration options?
-	busIdentifier := (uint64(0x5472697469756) << 4) | uint64(0xd)
-
-	binary.BigEndian.PutUint32(setupBuffer[0:4], fwdIdentifier)
-	binary.BigEndian.PutUint32(setupBuffer[4:8], fwdRange)
-	binary.BigEndian.PutUint64(setupBuffer[8:16], busIdentifier)
-	// TODO: We probably need error checking here in case the MAC address
-	// is not 8 bytes?
-	macAddress := networkInterface.HardwareAddr
-	copy(setupBuffer[16:24], macAddress[0:8])
-
-	// Establish a TCP connection
-	// TODO: Handle multiple Tritium bridges on the same subnet.
-	bridgeIPAddress := net.ParseIP(bridgeAddress)
-	conn, err := net.Dial("tcp4", fmt.Sprintf("%s:4876", bridgeIPAddress.String()))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// Send fwd identifiers for every message
-	// TODO: Maybe we want to support selectively sending fwd identifiers?
-	bytes, err := conn.Write(setupBuffer)
-	log.Println(bytes)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	b := make([]byte, 30)
-	// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
-	numBytes, err := io.ReadFull(conn, b[:])
-	log.Printf("Received %d bytes\n", numBytes)
-	if err != nil {
-		// error handling
-		log.Println(err)
-		return
-	}
-
-	if numBytes != 30 {
-		log.Println(b)
-		panic("Failed")
-	}
-
-	tritiumPacket := new(tritium.Packet)
-	tritium.ByteArrayToTritiumMessage(b, tritiumPacket)
-
-	log.Printf("CAN Id: %d\n", tritiumPacket.CanID)
-	log.Printf("Bus Number: 0x%x\n", tritiumPacket.BusNumber)
-	log.Printf("Client Identifier: 0x%x\n", tritiumPacket.ClientIdentifier)
-	log.Printf("Heartbeat: %t\n", tritiumPacket.FlagHeartbeat)
-	log.Printf("Settings: %t\n", tritiumPacket.FlagSettings)
-	log.Printf("RTR: %t\n", tritiumPacket.FlagRtr)
-	log.Printf("Extended: %t\n", tritiumPacket.FlagExtendedID)
-	log.Printf("Length: 0x%x\n", tritiumPacket.Length)
-	log.Printf("Data: 0x%x\n", tritiumPacket.Data)
-
-	// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
-	if !tritiumPacket.FlagHeartbeat {
-		sendFrame := make([]byte, 16)
-		tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
-		// Find the socket by bus number
-		unix.Write(fd, sendFrame)
-	}
-
-	go func() {
-		// Subsequent packets are 14 bytes
-		buff := make([]byte, 14)
-		for {
-			// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
-			numBytes, err := io.ReadFull(conn, buff[:])
-			log.Printf("Received %d bytes\n", numBytes)
-			for i, val := range buff {
-				log.Printf("buff[%d] = 0x%x\n", i, val)
-			}
-			if err != nil {
-				// error handling
-				log.Println(err)
-				continue
-				panic("Failed")
-			}
-
-			if numBytes != 14 {
-				log.Println(buff)
-				panic("Failed")
-			}
-
-			tritiumPacket := new(tritium.Packet)
-			tritium.ByteArrayTCPToTritiumMessage(buff, tritiumPacket)
-
-			// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
-			if !tritiumPacket.FlagHeartbeat {
-				sendFrame := make([]byte, 16)
-				tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
-				// Find the socket by bus number
-				unix.Write(fd, sendFrame)
-			}
-		}
-	}()
-
-	select {}
-}
-
-func doStuff() {
-	// Set GitCommit and Version
-	log.Println(bridge)
-
-	var conf Config
-
-	// TOML parsing
-	toml.DecodeFile("example.toml", &conf)
 	for _, bridge := range conf.Bridge {
 		vcan, err := net.InterfaceByName(bridge.SocketCANInterface)
 		if err != nil {
@@ -292,17 +182,124 @@ func doStuff() {
 			return
 		}
 
-		log.Printf("bridge.ID: 0x%x\n", bridge.ID)
-		log.Printf("bridge.IP: %s\n", bridge.IP)
-		log.Printf("bridge.NetworkInterface: %s\n", bridge.NetworkInterface)
-		log.Printf("bridge.SocketCANInterface: %s\n", bridge.SocketCANInterface)
+		setupBuffer := make([]byte, (4 + 4 + 1 + 7 + 1 + 7))
 
-		// TODO: Does Cobra have a native way of doing enumerated strings?
-		if Transport == "udp" {
-			doStuffOverUDP(fd, networkInterface)
-		} else if Transport == "tcp" {
-			doStuffOverTCP(fd, networkInterface)
+		// The bridge will forward any packet matching:
+		//
+		// 		fwdIdentifier <= CAN arbitration id < (fwdIdentifier + fwdRange)
+		//
+		// So we set the bridge to forward all valid CAN IDs (including extended),
+		// which is the range [0, 2^29 - 1]
+		fwdIdentifier := uint32(0)
+		fwdRange := uint32(536870911)
+		// The Bus Number must match the Bus Number specified in the Tritium
+		// CAN-Ethernet Bridge Configuration tool, otherwise the TCP connection is
+		// terminated.
+		// TODO: Take these as configuration options?
+		busIdentifier := (uint64(0x5472697469756) << 4) | uint64(0xd)
+
+		binary.BigEndian.PutUint32(setupBuffer[0:4], fwdIdentifier)
+		binary.BigEndian.PutUint32(setupBuffer[4:8], fwdRange)
+		binary.BigEndian.PutUint64(setupBuffer[8:16], busIdentifier)
+		// TODO: We probably need error checking here in case the MAC address
+		// is not 8 bytes?
+		macAddress := networkInterface.HardwareAddr
+		copy(setupBuffer[16:24], macAddress[0:8])
+
+		// Establish a TCP connection
+		// TODO: Handle multiple Tritium bridges on the same subnet.
+		bridgeIPAddress := net.ParseIP(bridge.IP)
+		conn, err := net.Dial("tcp4", fmt.Sprintf("%s:4876", bridgeIPAddress.String()))
+		if err != nil {
+			log.Println(err)
+			return
 		}
+
+		// Send fwd identifiers for every message
+		// TODO: Maybe we want to support selectively sending fwd identifiers?
+		bytes, err := conn.Write(setupBuffer)
+		log.Println(bytes)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Start a goroutine for each bridge we're receiving from
+		go func(conn net.Conn, fd int) {
+			b := make([]byte, 30)
+			// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
+			numBytes, err := io.ReadFull(conn, b[:])
+			if err != nil {
+				// error handling
+				log.Println(err)
+				return
+			}
+
+			if numBytes != 30 {
+				log.Println(b)
+				panic("Failed")
+			}
+
+			tritiumPacket := new(tritium.Packet)
+			tritium.ByteArrayToTritiumMessage(b, tritiumPacket)
+
+			// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
+			if !tritiumPacket.FlagHeartbeat {
+				sendFrame := make([]byte, 16)
+				tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
+				// Find the socket by bus number
+				unix.Write(fd, sendFrame)
+			}
+
+			// Subsequent packets are 14 bytes
+			buff := make([]byte, 14)
+			for {
+				// (64 + 8 + 8 + 32 + 56 + 8 + 56 + 8) bits = 30 bytes
+				numBytes, err := io.ReadFull(conn, buff[:])
+				log.Printf("Received %d bytes\n", numBytes)
+				for i, val := range buff {
+					log.Printf("buff[%d] = 0x%x\n", i, val)
+				}
+				if err != nil {
+					// error handling
+					log.Println(err)
+					continue
+					panic("Failed")
+				}
+
+				if numBytes != 14 {
+					log.Println(buff)
+					panic("Failed")
+				}
+
+				tritiumPacket := new(tritium.Packet)
+				tritium.ByteArrayTCPToTritiumMessage(buff, tritiumPacket)
+
+				// Now forward onto SocketCAN interface if it isn't a Heartbeat frame
+				if !tritiumPacket.FlagHeartbeat {
+					sendFrame := make([]byte, 16)
+					tritium.PacketToSocketCANFrame(tritiumPacket, sendFrame)
+					// Find the socket by bus number
+					unix.Write(fd, sendFrame)
+				}
+			}
+		}(conn, fd)
+
 	}
 
+	select {}
+}
+
+func doStuff() {
+	// Set GitCommit and Version
+	var conf Config
+
+	// TOML parsing
+	toml.DecodeFile("example.toml", &conf)
+
+	if Transport == "udp" {
+		doStuffOverUDP(conf)
+	} else if Transport == "tcp" {
+		doStuffOverTCP(conf)
+	}
 }
