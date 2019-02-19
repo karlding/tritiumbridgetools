315599e759fff41ef189932ea2464eff266b0ba4
Karl <karlding@users.noreply.github.com>
2019-02-18 21:04:12 -0500

    Add test for packing messages using extended ids
    Add a test for packing messages using extended ids, taken from the ELCON
    UHF charger's Status Message.

--- pkg/tritium/packet_test.go
+++ pkg/tritium/packet_test.go
@@ -52,6 +52,7 @@ func TestByteArrayToTritiumMessageHeartbeat(t *testing.T) {
 }
 
 func TestPacketToNetworkByteArrayStandardMsg(t *testing.T) {
+	// Taken from MS XII BMS Battery Voltage/Temperature Message
 	canPacket := Packet{
 		VersionIdentifier: uint64(0x5472697469756),
 		BusNumber:         0xd,
@@ -71,7 +72,31 @@ func TestPacketToNetworkByteArrayStandardMsg(t *testing.T) {
 
 	PacketToNetworkByteArray(&canPacket, networkBuffer)
 
-	assert.Equal(t, expectedBuffer, networkBuffer)
+	assert.Equal(t, expectedBuffer, networkBuffer, "Buffer was not equal")
+}
+
+func TestPacketToNetworkByteArrayExtendedMsg(t *testing.T) {
+	// Taken from ELCON UHF Charger Status Message
+	canPacket := Packet{
+		VersionIdentifier: uint64(0x5472697469756),
+		BusNumber:         0xd,
+		ClientIdentifier:  uint64(0xf8000c0a),
+		CanID:             uint32(0x18ff50e5),
+		FlagExtendedID:    true,
+		Length:            8,
+		Data:              uint64(0x6400640000000000), // TODO: Change this to an array?
+	}
+	networkBuffer := make([]byte, 30)
+	expectedBuffer := []byte{
+		0x00, 0x54, 0x72, 0x69, 0x74, 0x69, 0x75, 0x6d,
+		0x00, 0x00, 0x00, 0x00, 0xf8, 0x00, 0x0c, 0x0a,
+		0x18, 0xff, 0x50, 0xe5, 0x01, 0x08, 0x64, 0x00,
+		0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+
+	PacketToNetworkByteArray(&canPacket, networkBuffer)
+
+	assert.Equal(t, expectedBuffer, networkBuffer, "Buffer was not equal")
 }
 
 func TestByteArrayIdentity(t *testing.T) {
